7b22b3ae2850f8c4bc8af412105b978e6b183db8
shanderb <s.hander8910@hotmail.com>
2025-04-27 21:43:45 -0300

    Refactor background script for improved readability and add content scripts section in manifest

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
 // Adiciona um listener para o clique no ícone da extensão
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // Verifica se a URL é do Reddit
+    if (!tab.url.includes("reddit.com")) {
+        return;
+    }
     // Codifica a URL da aba atual
     const url = encodeURIComponent(tab.url);
     // Monta a URL de redirecionamento para o Rapidsave
     const redirectUrl = `https://rapidsave.com/info?url=${url}`;
     // Abre uma nova aba com a URL do Rapidsave
-    chrome.tabs.create({ url: redirectUrl }, function(newTab) {
+    chrome.tabs.create({ url: redirectUrl }, function (newTab) {
         // Adiciona um listener para detectar quando a nova aba terminar de carregar
         chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
             // Verifica se é a aba correta e se o carregamento foi concluído
